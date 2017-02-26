cb7e6d2f82bdfb4caf7f36cf60b0f7f48dc52f03
Joshua Haertel <josh.haertel@gmail.com>
2017-02-25 17:08:48 -0700

    PEP8ing PSoC

--- nodes/controller/psoc.py
+++ nodes/controller/psoc.py
@@ -3,12 +3,13 @@ import serial
 
 ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=None)
 
-SAMPLE_RATE = 50 #samples per second
-PULSES_PER_ROTATION = 4955 #Old motors
+SAMPLE_RATE = 50  # samples per second
+PULSES_PER_ROTATION = 4955  # Old motors
 
 motor_1_speed = 0
 motor_2_speed = 0
 motor_3_speed = 0
+
 
 def spin():
     ser.write('s')
@@ -16,11 +17,14 @@ def spin():
     writeFloat(motor_2_speed)
     writeFloat(motor_3_speed)
 
+
 def writeFloat(f):
     ser.write(struct.pack('>i', int(f*1000)))
 
+
 def readFloat():
     return float(struct.unpack('>i', ser.read(4))[0])/1000
+
 
 def setPower(p1, p2, p3):
     ser.write('p')
@@ -28,28 +32,29 @@ def setPower(p1, p2, p3):
     writeFloat(p2)
     writeFloat(p3)
 
-def set_motor_speed(motor_num, speed):
-    if motor_num == 1:
-        global motor_1_speed
-        motor_1_speed = speed
-    elif motor_num == 2:
-        global motor_2_speed
-        motor_2_speed = speed
-    elif motor_num == 3:
-        global motor_3_speed
-        motor_3_speed = speed
 
-def setPID(motor, p, i, qpps): #use motor = 0 to set all motors
+def set_motor_speeds(speeds):
+    global motor_1_speed
+    global motor_2_speed
+    global motor_3_speed
+    motor_1_speed = speeds[0]
+    motor_2_speed = speeds[1]
+    motor_3_speed = speeds[2]
+
+
+def setPID(motor, p, i, qpps):  # use motor = 0 to set all motors
     ser.write('k')
     ser.write(str(motor))
     writeFloat(p)
     writeFloat(i)
     writeFloat(qpps)
 
+
 def setT(period_ms, tau_ms):
     ser.write('t')
     writeFloat(period_ms)
     writeFloat(tau_ms)
+
 
 def getSpeed(wheel_num=0):
     """
@@ -69,8 +74,10 @@ def getEncoderCount():
     ser.write('e')
     return readFloat(), readFloat(), readFloat()
 
+
 def disengage():
     ser.write('d')
+
 
 setT(20, 50)
 
--- nodes/controller/wheel.py
+++ nodes/controller/wheel.py
@@ -5,24 +5,24 @@ import numpy
 def get_default_wheel(wheel_num):
     return Wheel(wheel_num)
 
+
 def get_default_wheel_list():
     result = []
     for i in range(3):
         result.append(get_default_wheel(i))
     return result
 
-def set_motor_speed(speed_list):
-    psoc.
 
-def spin():
-    psoc.spin()
+def set_motor_speed(speed_list):
+    psoc.set_motor_speeds(speed_list)
+
 
 class Wheel(object):
     def __init__(self, wheel_num):
         self.wheel_num = wheel_num
         # self.linear_velocity = linear_velocity
         # self.wheel_spin = wheel_spin = 0
-        psoc.setPID(self.wheel_num, 1, 1, 9000) # 9000
+        psoc.setPID(self.wheel_num, 1, 1, 9000)  # 9000
 
     def get_speed(self):
         """
