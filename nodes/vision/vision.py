3e8f3ece3788367a0462404067c9d89ab23a22b9
msanch <martinsanchez2010@gmail.com>
2017-02-28 17:25:01 -0700

    great vision for calibrating and tracking the ball and the robot

--- nodes/vision/vision.py
+++ nodes/vision/vision.py
@@ -32,19 +32,27 @@ class Vision:
         np.array([50, 255, 255])
     ]
 
-    ball_range = [
-        np.array([85, 10, 100]),
-        np.array([105, 255, 255])
+    ball_range = [ # blue
+        np.array([73, 4, 215]),
+        np.array([113, 44, 255])
     ]
 
-    created = dict()
+    current_range = ball_range
+
+    _x_min = 85
+    _x_max = 750
+    _y_min = 30
+    _y_max = 465
+
+    min_hsv = [0, 0, 0]
+    max_hsv = [179, 255, 255]
+
 
     def __init__(self):
         self.bridge = CvBridge()
         rospy.Subscriber('/usb_cam_away/image_raw', Image, self.receive_frame)
         self.ally1_pub = rospy.Publisher('psp_ally1', Pose2D, queue_size=10)
         self.ball_pub = rospy.Publisher('psp_ball', Pose2D, queue_size=10)
-
         # self.init_window('Ball', self.ball_range)
 
     def init_window(self, name, color_range):
@@ -76,25 +84,37 @@ class Vision:
     def calibration_callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             hsv_pixel = self.hsv_image[y][x]
-            print hsv_pixel
-            self.ball_range[0] = hsv_pixel-10
-            self.ball_range[1] = hsv_pixel+10
+            self.current_range[0] = np.array([max(val-20, m) for val, m in zip(hsv_pixel, self.min_hsv)])
+            self.current_range[1] = np.array([min(val+20, m) for val, m in zip(hsv_pixel, self.max_hsv)])
+            print self.ball_range
 
     def receive_frame(self, image):
         window_name = "Calibration Window"
         self.bgr_image = self.bridge.imgmsg_to_cv2(image, 'bgr8')
+        self.bgr_image = np.array([np.array(row[self._x_min:self._x_max+1]) for row in self.bgr_image[self._y_min:self._y_max+1]])
         self.hsv_image = cv2.cvtColor(self.bgr_image, cv2.COLOR_BGR2HSV)
+        self.process_ball('Ball', self.ball_range, self.ball_pub)
+        x, y, a = self.process_robot('Ally 1', self.ally1_range, self.ally1_pub)
         cv2.imshow(window_name, self.bgr_image)
         cv2.setMouseCallback(window_name, self.calibration_callback)
-        self.process_ball('Ball', self.ball_range, self.ball_pub)
-        cv2.waitKey(1)
-        # self.process_robot('Ally 1', self.ally1_range, self.ally1_pub)
+        # cv2.setKeyboardCallback(window_name, self.keyboard_callback)
+        char = cv2.waitKey(1)
+        if char != -1:
+            print x, y, a
+        self.set_current_range(char)
 
     def calibrate_parameter(self, color_range, index_1, index_2, r, m, new_value):
         color_range[index_1][index_2] = new_value
 
+    def set_current_range(self, char):
+        if char == ord('b'):
+            self.current_range = self.ball_range
+        elif char == ord('1'):
+            self.current_range = self.ally1_range
+
     def process_ball(self, name, color_range, pub):
         mask = cv2.inRange(self.hsv_image, color_range[0], color_range[1])
+        mask = cv2.GaussianBlur(mask, (3, 3), 0)
 
         im2, contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -106,30 +126,21 @@ class Vision:
 
         if len(contour_moments) < 1:
             cv2.imshow(name, mask)
-            # cv2.waitKey(1)
             return
 
         contour_moment = max(contour_moments, key=lambda m: m['m00'])
 
         x, y = [int(x) for x in self.get_center_of_mass(contour_moment)]
-        cv2.rectangle(mask, (x-5, y-5), (x+5, y+5), (0,255,0), -1)
-        if not self.created:
-            # cv2.createTrackbar('Hue Min', name, color_range[0][0], 179, lambda v: v) # self.calibrate_parameter(color_range, 0, 0, 10, 179, v))
-            # cv2.createTrackbar('Hue Max', name, color_range[1][0], 179, lambda v: v) # self.calibrate_parameter(color_range, 0, 1, 10, 179, v))
-            # cv2.createTrackbar('Sat Min', name, color_range[0][1], 255, lambda v: v) # self.calibrate_parameter(color_range, 1, 0, 100, 255, v))
-            # cv2.createTrackbar('Sat Max', name, color_range[1][1], 255, lambda v: v) # self.calibrate_parameter(color_range, 1, 1, 100, 255, v))
-            # cv2.createTrackbar('Val Min', name, color_range[0][2], 255, lambda v: v) # self.calibrate_parameter(color_range, 2, 0, 50, 255, v))
-            # cv2.createTrackbar('Val Max', name, color_range[1][2], 255, lambda v: v) # self.calibrate_parameter(color_range, 2, 1, 50, 255, v))
-            created = True
+        cv2.rectangle(self.bgr_image, (x-5, y-5), (x+5, y+5), (0,255,0), -1)
         cv2.imshow(name, mask)
-        # cv2.waitKey(5)
 
         pub.publish(Pose2D(x=x, y=y, theta=0))
 
     def process_robot(self, name, color_range, pub):
         mask = cv2.inRange(self.hsv_image, color_range[0], color_range[1])
+        mask = cv2.GaussianBlur(mask, (3, 3), 0)
 
-        im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        im2, contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
         contour_moments = []
         for contour in contours:
@@ -139,7 +150,6 @@ class Vision:
 
         if len(contour_moments) < 2:
             cv2.imshow(name, mask)
-            # cv2.waitKey(1)
             return
 
         contour_moments.sort(key=lambda m: m['m00'])
@@ -147,9 +157,8 @@ class Vision:
 
         for mm in contour_moments:
             x, y = [int(x) for x in self.get_center_of_mass(mm)]
-            cv2.rectangle(mask, (x-5, y-5), (x+5, y+5), (0,255,0), -1)
+            cv2.rectangle(self.bgr_image, (x-5, y-5), (x+5, y+5), (0,255,0), -1)
         cv2.imshow(name, mask)
-        # cv2.waitKey(1)
 
         mm_large = contour_moments[-1]
         mm_small = contour_moments[-2]
@@ -157,12 +166,9 @@ class Vision:
         s_x, s_y = self.image_to_world_coordinates(*self.get_center_of_mass(mm_small))
         x, y = (l_x + s_x) / 2, (l_y + s_y) / 2
 
-
-        # this returns in radians
-        # Joshua: I want radians
         angle = math.atan2(s_y - l_y, s_x - l_x)
-        # print x, y, angle
         pub.publish(Pose2D(x=x, y=y, theta=angle))
+        return x, y, angle
 
 
 def main():
