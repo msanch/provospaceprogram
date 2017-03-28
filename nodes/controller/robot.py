9b7d21f428920dc408f01fc34fe100a0320916a4
msanch <martinsanchez2010@gmail.com>
2017-03-28 10:01:11 -0600

    Changing robot speed smoothing

--- nodes/controller/robot.py
+++ nodes/controller/robot.py
@@ -13,26 +13,29 @@ import kinematic
 import wheel
 
 
-def _calculate_optimum_velocity(d_x, limit):
+def _limit_speed(d_x, limit):
     result = d_x
     if d_x > 0 and d_x > limit:
         result = limit
-    elif d_x < 0:
-        result = -1 * d_x
-        neg_limit = -1 * limit
-        if d_x < neg_limit:
-            result = neg_limit
+    elif d_x < 0 and d_x < -limit:
+        result = -limit
     return result
 
 
 class Robot(object):
+    # FIXME FINE TUNING : Set these optimal speeds
+    MAX_ROBOT_SPEED = 2.0
+    MIN_ROBOT_SPEED = 1.0
+    CEILING_FLOOR_CUTOFF = 0.1
+    OPTIMAL_ROBOT_ANGULAR_SPEED = math.pi/4
+
     def __init__(self, velocity=(0, 0), angular_velocity=0,
                  wheels=wheel.get_default_wheel_list(), position=(0, 0),
                  theta=0):
         """
         wheels : [Wheel(), Wheel(), Wheel()]
         """
-        self.boost_range = (0.1, 1.0)
+        self.speed_limits = (self.CEILING_FLOOR_CUTOFF, self.MIN_ROBOT_SPEED, self.MAX_ROBOT_SPEED)
         self.velocity = velocity
         self.angular_velocity = angular_velocity  # omega, w
         if len(wheels) != 3:
@@ -61,19 +64,10 @@ class Robot(object):
         self.current_position = (msg.x, msg.y)
         self.current_theta = -1 * msg.theta
 
- 
-    # FIXME FINE TUNING : Set these optimal speeds
-    OPTIMAL_ROBOT_SPEED = 2.0
-    OPTIMAL_ROBOT_ANGULAR_SPEED = math.pi
-
     def _get_velocities(self, d_x, d_y, d_t):
-        result = []
-        result.append(_calculate_optimum_velocity(d_x,
-                                                  self.OPTIMAL_ROBOT_SPEED))
-        result.append(_calculate_optimum_velocity(d_y,
-                                                  self.OPTIMAL_ROBOT_SPEED))
-        result.append(_calculate_optimum_velocity(d_t,
-                                                  self.OPTIMAL_ROBOT_ANGULAR_SPEED))
+        result = [0, 0, 0]
+        result[0], result[1] = self._smooth_speed(d_x, d_y)
+        result[2] = _limit_speed(d_t, self.OPTIMAL_ROBOT_ANGULAR_SPEED)
         return result
 
     def _get_wheel_speed_list(self):
@@ -83,37 +77,24 @@ class Robot(object):
             result.append(speed)
         return result
 
-#    MINIMUM_ROTATION_NORMALIZATION_THRESHOLD = 1
-    def normalize_wheel_speed(self, desired_wheel_velocity_list):
-        result = []
-        for velocity in desired_wheel_velocity_list:
-            velocity = velocity / 1.5
-            #if velocity < 0.1:
-            #    velocity = 1.0
-            result.append(velocity)
-        return result
+
+    def _smooth_speed(self, delta_x, delta_y):
+        dist = math.sqrt(delta_x**2 + delta_y**2)
+        if self.speed_limits[0] < dist < self.speed_limits[1]:
+            delta_x /= dist / self.speed_limits[1]
+            delta_y /= dist / self.speed_limits[1]
+        elif self.speed_limits[2] < dist:
+            delta_x /= dist / self.speed_limits[2]
+            delta_y /= dist / self.speed_limits[2]
+        return delta_x, delta_y
 
     i = 0
     def run(self):
         delta_x = (self.current_position[0] - self.desired_position[0])
         delta_y = (self.current_position[1] - self.desired_position[1])
         delta_theta = (self.current_theta - self.desired_theta)
-        # Ben's stabilizer
-        dist = math.sqrt(delta_x**2 + delta_y**2)
-        if self.boost_range[0] < dist < self.boost_range[1]:
-            delta_x /= dist / self.boost_range[1]
-            delta_y /= dist / self.boost_range[1]
-        # end Ben's stabilizer
-        # velocity_list = self._get_velocities(delta_x, delta_y, delta_theta)
-        velocity_list = [delta_x, delta_y, delta_theta]
+        velocity_list = self._get_velocities(delta_x, delta_y, delta_theta)
         desired_wheel_velocity_list = kinematic.get_desired_wheel_speeds(self.current_theta, velocity_list)
-#        normalizer = abs(max(desired_wheel_velocity_list, key=lambda x: abs(x))) / 1.5
-#        if normalizer != 0:
-#            desired_wheel_velocity_list = [v/normalizer for v in desired_wheel_velocity_list]
-#        normalizer = 200
-#        desired_wheel_velocity_list = [v/normalizer for v in desired_wheel_velocity_list]
-
-        desired_wheel_velocity_list = self.normalize_wheel_speed(desired_wheel_velocity_list)
         # if self.i == 100:
         #     self.i = 0
         #     print "Currrent: ", self.current_position, self.current_theta
@@ -125,7 +106,6 @@ class Robot(object):
 
 def main():
     print "Starting Robot Controller Node"
-    # FIXME ROSPY : Valid name here?
     if not debug:
         rospy.init_node("psp_controller", anonymous=False)
     robot = Robot()
