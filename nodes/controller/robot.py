9b9cc246e749432f3643cde002069ee83cb35eb4
Joshua Haertel <josh.haertel@gmail.com>
2017-04-05 11:02:10 -0600

    robot bugs

--- nodes/controller/robot.py
+++ nodes/controller/robot.py
@@ -6,12 +6,13 @@ import sys
 if debug:
     import time
 else:
-    import rospy
     from geometry_msgs.msg import Pose2D
+    import rospy
 
 import kinematic
 import wheel
-
+from soccerref.msg import GameState as GameStateMsg
+from ai.soccerobjects import GameState
 
 def _limit_speed(d_x, limit):
     result = d_x/1.5
@@ -41,14 +42,17 @@ class Robot(object):
     MIN_ROBOT_SPEED = 0.3
     CEILING_FLOOR_CUTOFF = 0.07
     MAXIMUM_ANGULAR_SPEED = math.pi/1
+    game_state = GameState()
+    is_team_home = True
 
-    def __init__(self, psp_number, velocity=(0, 0), angular_velocity=0,
+    def __init__(self, psp_number, is_team_home=True, velocity=(0, 0), angular_velocity=0,
                  wheels=wheel.get_default_wheel_list(), position=(0, 0),
                  theta=0):
         """
         wheels : [Wheel(), Wheel(), Wheel()]
         """
         self.psp_number = psp_number
+        self.is_team_home = is_team_home
         self.speed_limits = (self.CEILING_FLOOR_CUTOFF, self.MIN_ROBOT_SPEED, self.MAX_ROBOT_SPEED)
         self.velocity = velocity
         self.angular_velocity = angular_velocity  # omega, w
@@ -60,7 +64,7 @@ class Robot(object):
         self.desired_position = position  # (10.0, 0.0)
         self.desired_theta = theta  # math.pi/2 # 3.14159
         self.wheels[0].set_debug(debug)
-        if not debug:
+        if debug:
             desired_topic = "/provospaceprogram_home/desired_skills_state%d" % psp_number if not keyboard else "/psp_keyboard_desired"
             print desired_topic
             current_topic = "/provospaceprogram_home/ally%d_estimator" % psp_number
@@ -68,6 +72,7 @@ class Robot(object):
                              self.handle_desired_state)
             rospy.Subscriber(current_topic, Pose2D,
                              self._handle_current_state)
+            rospy.Subscriber('/game_state', GameStateMsg, self.game_state.update)
 
     def handle_desired_state(self, msg):
         # print "Handling desired"
@@ -108,22 +113,23 @@ class Robot(object):
             delta_y /= dist / self.speed_limits[2]
         return delta_x, delta_y
 
-    # i = 0
+    i = 0
     def run(self):
-        delta_x = (self.current_position[0] - self.desired_position[0])
-        delta_y = (self.current_position[1] - self.desired_position[1])
+        # side = 1 if self.is_team_home ^ self.game_state.second_half else -1
+        delta_x = (self.current_position[0] - self.desired_position[0]) # * side
+        delta_y = (self.current_position[1] - self.desired_position[1]) # * side
         # delta_theta = (self.current_theta - self.desired_theta)
         delta_theta = (self.desired_theta - self.current_theta)
         velocity_list = self._get_velocities(delta_x, delta_y, delta_theta)
         desired_wheel_velocity_list = kinematic.get_desired_wheel_speeds(self.current_theta, velocity_list)
-        # if self.i == 100:
-        #     self.i = 0
-        #     print "Currrent: ", self.current_position, self.current_theta
-        #     print "Desired: ", self.desired_position, self.desired_theta
-        #     print "V List: ", velocity_list
-        #     print "Rot/s: ", desired_wheel_velocity_list
-        #     print math.sqrt(delta_x**2 + delta_y**2), '\n'
-        # self.i += 1
+        if self.i == 100:
+            self.i = 0
+            print "Currrent: ", self.current_position, self.current_theta
+            print "Desired: ", self.desired_position, self.desired_theta
+            print "V List: ", velocity_list
+            print "Rot/s: ", desired_wheel_velocity_list
+            print math.sqrt(delta_x**2 + delta_y**2), '\n'
+        self.i += 1
         wheel.set_motor_speed(desired_wheel_velocity_list)
 
 def main():
@@ -131,7 +137,8 @@ def main():
     psp_number = int(sys.argv[1])
     if not debug:
         rospy.init_node("psp%d_controller" % psp_number, anonymous=False)
-    robot = Robot(psp_number)
+    # is_team_home = sys.argv[2] == "home"  # rospy.get_param('~is_team_home')
+    robot = Robot(psp_number)  # , is_team_home)
     rate = 1/100 if debug else rospy.Rate(100)  # 100 Hz
     try:
         def condition(): return None if debug else rospy.is_shutdown()
